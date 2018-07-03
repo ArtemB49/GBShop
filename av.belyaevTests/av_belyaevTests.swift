0b781b12b965a637d28db14461609fc3eb507455
Артем Беляев <>
2018-07-03 15:39:41 +0500

    init

---
+++ av.belyaevTests/av_belyaevTests.swift
@@ -0,0 +1,36 @@
+//
+//  av_belyaevTests.swift
+//  av.belyaevTests
+//
+//  Created by Артем Б on 03.07.2018.
+//  Copyright © 2018 Артем Б. All rights reserved.
+//
+
+import XCTest
+@testable import av_belyaev
+
+class av_belyaevTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
